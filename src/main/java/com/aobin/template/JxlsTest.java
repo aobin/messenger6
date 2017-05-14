0a75a91695d5140b0d9105eb1369b9f817d1dded
aobin <aobin@beyondsoft.com>
2017-05-14 20:32:57 +0800

    添加JXLS的基本配置

---
+++ src/main/java/com/aobin/template/JxlsTest.java
@@ -0,0 +1,51 @@
+package com.aobin.template;
+
+import com.aobin.messenger.models.Student;
+import com.aobin.messenger.services.StudentService;
+import com.google.common.io.Resources;
+import org.jxls.common.Context;
+import org.jxls.util.JxlsHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+/**
+ * Created by 敖斌 on 2017/5/14.
+ */
+public class JxlsTest
+{
+    private static final Logger logger = LoggerFactory.getLogger(JxlsTest.class);
+
+    private static List<Student> students = new StudentService().getStudents();
+
+    public static void main(String[] args)
+    {
+        InputStream inputStream = null;
+        for(Student student:students)
+        {
+            System.out.println(student);
+        }
+
+        try
+        {
+            //直接获取resources目录下面的文件,没有/
+            inputStream = Resources.getResource("template/xls_template.xlsx").openStream();
+            OutputStream os = new FileOutputStream("D:\\xls_template_output.xlsx");
+            Context context = new Context();
+            context.putVar("students", students);
+            JxlsHelper.getInstance().processTemplate(inputStream,os,context);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+}
